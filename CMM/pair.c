59984cd679d20815da9e5940a3706b38f0360456
Temennigru <Jean@Jean-Lucs-MacBook-Pro.local>
2014-06-18 18:42:20 -0300

    Adding documentation to pair.c and semaphore.c

--- CMM/pair.c
+++ CMM/pair.c
@@ -1,6 +1,16 @@
 #include "pair.h"
 #include <stdlib.h>
 
+/* Method: MakePair
+ * Parameters:
+ * first - First element of the pair
+ * second - Second element of the pair
+ * Return value:
+ * Returns pointer to created pair.
+ * Description:
+ * Allocates a pair, adds first and second to it and returns the pointer.
+ */
+
 Pair* MakePair (void* first, void* second) {
 	Pair* ret = (Pair*) malloc (sizeof (Pair) );
 
