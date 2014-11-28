6ba92b12fcf9c9268a5a3d89a22548527e6697f3
Temennigru <temennigru@github.com>
2014-11-28 18:58:47 -0200

    Adding assertprintf

--- CMM/util.c
+++ CMM/util.c
@@ -4,12 +4,43 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdarg.h>
+#include <execinfo.h>
+#include <stdlib.h>
+
+
+void assertprintf(int asst, const char* format, ...) {
+    #if defined(NDEBUG) && defined(__GNUC__)
+    #else
+        if (!asst) {
+            va_list argptr;
+            size_t size;
+            void *buffer[5];
+            char **strings;
+            size_t i;
+
+            size = backtrace (buffer, 5);
+            strings = backtrace_symbols (buffer, size);
+            va_start(argptr, format);
+            for (i = 0; i < size; ++i) {
+                 fprintf(stderr, "%s\n", strings[i]);
+             }
+            vfprintf(stdout, format, argptr);
+            fprintf(stdout, "\n");
+            va_end(argptr);
+            abort();
+        }
+    #endif
+}
+
 
 void debugprintf(const char* format, ...) {
-    va_list argptr;
-    va_start(argptr, format);
-    assert(vfprintf(stdout, format, argptr) >= 0);
-    va_end(argptr);
+    #if defined(NDEBUG) && defined(__GNUC__)
+    #else
+        va_list argptr;
+        va_start(argptr, format);
+        vfprintf(stdout, format, argptr);
+        va_end(argptr);
+    #endif
 }
 
 #endif
--- CMM/util.h
+++ CMM/util.h
@@ -1,3 +1,18 @@
+#include "bool.h"
+
+/* Method: assertprintf
+ * Parameters:
+ * asst - Assertion expression result
+ * format - Formatable string
+ * ... - Format args
+ * Return value:
+ * none
+ * Description:
+ * Works similarly to assert, but will print a customized error message and stack trace.
+ */
+
+void assertprintf(Bool asst, const char* format, ...);
+
 /* Method: debugprintf
  * Parameters:
  * format - Formatable string
